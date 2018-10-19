69cd7ac82d7a136dbe85ea39f8af67a9bf9d64f6
keschaefer <keschaefer@gmail.com>
2018-10-19 15:43:04 -0600

    made toggle for compose message functional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Message from './Components/Message';
+import MessageList from './Components/MessageList';
+import Toolbar from './Components/Toolbar';
 
 class App extends Component {
+  constructor() {
+    super()
+      this.state = {
+        messages: [],
+        composeMessage: true,
+      }
+    }
+
+  async componentDidMount() {
+      let result = await fetch("http://localhost:8082/api/messages")
+      let data =  await result.json()
+      console.log("data", data)
+      this.setState({
+          messages: data,
+        })
+      }  
+
+  
+  hideMessage = () => {
+    this.setState({composeMessage: !(this.state.composeMessage)}) 
+    }
+
+  patch = async (id, command, attribute, value) => {
+    var patch = {
+      messageIds: [id],
+      command: command,
+      [attribute]: value,
+    }
+
+    const response = await fetch("http://localhost:8082/api/messages") {
+      method: 'PATCH',
+      body: JSON.stringify(patch),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    
+      const posted = await response.json()
+      this.setState({
+        messages: posted
+      }) 
+    }
+  
+
+  markStarred = () => {
+    this.patch([event.target.id], 'read', 'read', true)
+  }  
+
   render() {
     return (
-      <div className="App">
+      <div className="App body">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <Toolbar hideMessage ={this.hideMessage} composeMessage={this.state.composeMessage}/>
+         <Message composeMessage={this.state.composeMessage}/>
+         <MessageList messages = {this.state.messages} markStarred = {this.markStarred}/>
+       
+         
         </header>
       </div>
     );
---
+++ src/Components/MessageList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const MessageList = (props) => {
+   return props.messages.map(i => {
+      return (
+         <div id = {i.id}>
+         <div className= {`row message ${i.read ? 'read' : 'unread'} ${i.selected ? 'selected' : ''}`}>
+            <div className="col-xs-1">
+               <div className="row">
+                  <div className="col-xs-2">
+                     <input type="checkbox"></input>
+                  </div>
+                  <div className="col-xs-2">
+                     <i onClick = {props.markStarred} className= {`star fa fa-star${i.starred ? '-o' : ''}`}></i>
+                  </div>
+               </div>
+            </div>
+            <div className="col-xs-11">
+               <span className="label label-warning">{i.labels[0]}</span>
+               <span className="label label-warning">{i.labels[1]}</span>
+               <span className="label label-warning">{i.labels[2]}</span>
+               <span>Subject: {i.subject}</span>
+            </div>
+         </div>
+         <div className="row message-body hidden">
+            <div className="col-xs-11 col-xs-offset-1">
+            {i.body}
+            </div>
+         </div>
+      </div>
+
+   )
+})
+}
+
+
+
+export default MessageList;
